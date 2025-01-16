f0090fe9ff2d29569400c74f49416b482bf8e228
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-01-16 19:33:35 +0300

    field

---
+++ Models/FieldViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Club.Models
+{
+    public class FieldViewModel
+    {
+        public int FieldId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public int ClubsId { get; set; }
+        public IFormFile? Photo { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace Club
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+            builder.Services.AddScoped<AthleteDbStorage>();
+            builder.Services.AddScoped<ClubsDbStorage>();
+            builder.Services.AddScoped<CompetitionDbStorage>();
+            builder.Services.AddScoped<FieldDbStorage>();
+            builder.Services.AddScoped<ManagerDbStorage>();
+            builder.Services.AddScoped<SponsorDbStorage>();
+            builder.Services.AddScoped<StaffDbStorage>();
+
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
