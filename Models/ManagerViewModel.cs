2e677a71e7abaa711c7c28f39762a065450bfb52
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-01-16 20:35:16 +0300

    manager

---
+++ Models/ManagerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Club.Models
+{
+    public class ManagerViewModel
+    {
+        public int ManagerId { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public int ClubsId { get; set; }
+    }
+}
