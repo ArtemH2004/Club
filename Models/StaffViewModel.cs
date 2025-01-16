07e42595c837c81d628efbe7e1e30439970bc145
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-01-16 20:55:01 +0300

    staff

---
+++ Models/StaffViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Club.Models
+{
+    public class StaffViewModel
+    {
+        public int StaffId { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public char Gender { get; set; }
+        [Required]
+        public string Job { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public int ClubsId { get; set; }
+    }
+}
